adcb6bfb50f6530f02cc40af8410ac86dd9f4897
Verbuotojas <bombonkes666>
2021-04-13 17:41:46 +0300

    test

--- front-end/src/components/Home.js
+++ front-end/src/components/Home.js
@@ -4,6 +4,7 @@ export default function Home() {
     return (
         <div className="container">
             <p>This is home page</p>
+            <h2>Labas vakaras</h2>
         </div>
     )
 }

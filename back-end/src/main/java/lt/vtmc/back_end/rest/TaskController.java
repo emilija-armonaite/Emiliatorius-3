4fed533c2ce81a9dbb16f00c2aae51773da70be3
Algimantas <valavicius.algimantas@gmail.com>
2021-04-14 07:32:29 +0300

    Added CrossOrigin quick fix

--- back-end/src/main/java/lt/vtmc/back_end/rest/TaskController.java
+++ back-end/src/main/java/lt/vtmc/back_end/rest/TaskController.java
@@ -7,6 +7,7 @@ import lt.vtmc.back_end.service.TaskService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping(value = "/api/tasks", produces = MediaType.APPLICATION_JSON_VALUE)
 public class TaskController {
 

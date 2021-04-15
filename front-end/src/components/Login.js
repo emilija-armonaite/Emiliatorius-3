36831f342a466a8e5f9d04c5165c44d935670e51
Verbuotojas <bombonkes666>
2021-04-15 20:10:05 +0300

    login nice looking

--- front-end/src/components/Login.js
+++ front-end/src/components/Login.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
-import Avatar from '@material-ui/core/Avatar';
+import Avatar from '@material-ui/icons/Cake';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-// import FormControlLabel from '@material-ui/core/FormControlLabel';
-// import Checkbox from '@material-ui/core/Checkbox';
-// import Link from '@material-ui/core/Link';
-// import Grid from '@material-ui/core/Grid';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
@@ -15,19 +15,34 @@ import Container from '@material-ui/core/Container';
 import { useHistory } from "react-router-dom"
 import Footer from './Footer';
 import axios from "axios";
-import {Link} from "react-router-dom"
+// import {Link} from "react-router-dom"
 import Home from './Home';
+import Paper from '@material-ui/core/Paper';
+
+
 
 const useStyles = makeStyles((theme) => ({
+  root: {
+    height: '100vh',
+  },
+  image: {
+    backgroundImage: 'url(https://source.unsplash.com/random)',
+    backgroundRepeat: 'no-repeat',
+    backgroundColor:
+      theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  },
   paper: {
-    marginTop: theme.spacing(8),
+    margin: theme.spacing(8, 4),
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
   },
   avatar: {
     margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
+    color: theme.palette.secondary.main,
+    
   },
   form: {
     width: '100%', // Fix IE 11 issue.
@@ -37,6 +52,7 @@ const useStyles = makeStyles((theme) => ({
     margin: theme.spacing(3, 0, 2),
   },
 }));
+
 
 
 
@@ -71,14 +87,16 @@ export default function SignIn() {
 
 
   return (
-    <Container component="main" maxWidth="xs">
+<Grid container component="main" className={classes.root}>
       <CssBaseline />
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign in
+      <Grid item xs={false} sm={4} md={7} className={classes.image} />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+         JOIN US
         </Typography>
         <form className={classes.form} noValidate onSubmit={Login}>
           <TextField
@@ -124,24 +142,20 @@ export default function SignIn() {
             Sign In
           
           </Button>
-          {/* <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2">
-                Forgot password?
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link href="#" variant="body2">
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid>
-          </Grid> */}
-        </form>
-      </div>
-      <Box mt={8}>
-        {/* <Copyright /> */}
-        <Footer/>
-      </Box>
-    </Container>
+
+
+
+
+
+
+           
+
+            <Box mt={5}>
+              <Footer />
+            </Box>
+          </form>
+        </div>
+      </Grid>
+    </Grid>
   );
 }

a430c6e483edbae01c9dd46611adb7c856b24be8
Verbuotojas <nedass@ymail.com>
2021-04-26 20:26:04 +0300

    console

--- front-end/src/components/AddProject.js
+++ front-end/src/components/AddProject.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
@@ -13,16 +13,38 @@ import { green } from '@material-ui/core/colors';
 import TextareaAutosize from '@material-ui/core/TextareaAutosize';
 import Tabs from '@material-ui/core/Tabs';
 
+ 
 export default function AddProject() {
     const [open, setOpen] = React.useState(false);
+
+    
+    const [nameNewProject, setName] = useState("");
+    const [newDesc, setDesc] = useState();
+
+    const writeName = e=>{
+        // console.log(`Typed => ${e.target.value}`);
+        setName(e.target.value);
+    }
+    const writeDesc = e=>{
+        // console.log(`Typed => ${e.target.value}`);
+        setDesc(e.target.value);
+    }
+
 
     const handleClickOpen = () => {
         setOpen(true);
     };
 
     const handleClose = () => {
+        
         setOpen(false);
+      
     };
+    const submitB = () =>{
+        console.log({nameNewProject});
+        console.log({newDesc});
+        setOpen(false);
+    }
 
     return (
         <div>
@@ -41,7 +63,15 @@ export default function AddProject() {
                         id="projectName"
                         label="Project name"
                         type="text"
+                        value={nameNewProject}
+                        onChange ={writeName}
+
                         fullWidth
+                        // value={this.state.name}
+                        // onChange={e => this.setState({ name: e.target.value })}
+                    //    nameProject(value);
+
+                    // onChange={e=>setName({e})}
                     />
                     {/* <TextareaAutosize
                         rowsMax={4}
@@ -59,26 +89,29 @@ export default function AddProject() {
                         multiline
                         fullWidth
                         variant="outlined"
+                        value={newDesc}
+                        onChange ={writeDesc}
                     />
                 </DialogContent>
 
                 <DialogActions>
 
-                    <Fab onClick={handleClose} size="medium" color="secondary" aria-label="exit">
+                    {/* <Fab onClick={handleClose} size="medium" color="secondary" aria-label="exit">
                         <RemoveIcon />
                     </Fab>
 
                     <Fab onClick={handleClose} size="medium" color="primary" aria-label="add">
                         <AddIcon />
-                    </Fab>
+                    </Fab> */}
 
 
-                    {/* <Button onClick={handleClose} color="primary">
+                    <Button onClick={handleClose} color="primary">
                         Cancel
           </Button>
-                    <Button onClick={handleClose} color="primary">
+                    <Button onClick={submitB} color="primary">
                         Subscribe
-          </Button> */}
+                        
+          </Button>
                 </DialogActions>
             </Dialog>
         </div>

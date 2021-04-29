7de504564cc828a293452f88713f85aa8cfc322f
Emilija <emilija.e@gmail.com>
2021-04-29 23:38:48 +0300

    fix

--- front-end/src/components/project/DeleteProject.js
+++ front-end/src/components/project/DeleteProject.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 const DeleteProject = ({ id }) => {
 
     const user = JSON.parse(localStorage.getItem("token"));
+
     axios.delete(`http://localhost:8081/api/projects/${id}`, {
         headers: {
             "Authorization": "Bearer " + user.token
@@ -11,6 +12,7 @@ const DeleteProject = ({ id }) => {
     })
         .then(response => {
             console.log(response.data)
+            window.location.reload(true);
         })
         .catch(err => {
             console.log(err);
